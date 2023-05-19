1eb795181822aacb1353b5c3fd6514def71036f2
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-19 15:05:50 +0200

    f

--- views/users/signin.php
+++ views/users/signin.php
@@ -1,8 +1,23 @@
 <main>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                
+            <p>Connectez-vous :</p>
+            <form>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Adresse mail :</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">Nous ne partagerons pas votre email.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Mot de passe :</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Se souvenir de moi</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Valider</button>
+            </form>
             </div>
         </div>
     </div>
---
+++ views/users/signup.php
@@ -0,0 +1,28 @@
+<main>
+    <div class="container">
+        <div class="row">
+            <p>Inscrivez-vous :</p>
+            <form>
+            <div class="mb-3">
+                    <label for="email" class="form-label">Nom :</label>
+                    <input type="email" class="form-control" id="email" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Prénom :</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Adresse mail :</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" required>
+                    <div id="emailHelp" class="form-text">Nous ne partagerons pas votre email.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Mot de passe :</label>
+                    <input type="password" class="form-control" id="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Valider</button>
+            </form>
+            </div>
+        </div>
+    </div>
+</main>
