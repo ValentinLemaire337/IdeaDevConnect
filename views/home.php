7701276f33fe1e894e2a058e77092ad68f60b2f0
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-19 10:51:46 +0200

    f

--- views/home.php
+++ views/home.php
@@ -7,6 +7,8 @@
                 <p>Le but de ce projet est de vous permettre de proposer ou de contribuer à des projets selon vos compétences.</p>
                 <p>Inscrivez-vous, visitez notre forum et postulez sur une offre ou créez la votre pour commencer à recevoir des demandes d'autres membres pour rejoindre votre projet !</p>
             </div>
+        </div>
+        <div class="row">
 
         </div>
     </div>
