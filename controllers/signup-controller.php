408cc293aae94cdfca44a2d9407d3a9785b3d0d0
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-19 16:15:00 +0200

    f

---
+++ controllers/signup-controller.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/../config/default.php';
+
+
+include __DIR__ . '/../views/templates/header.php';
+    include __DIR__ . '/../views/users/signin.php';
+include __DIR__ . '/../views/templates/footer.php';
