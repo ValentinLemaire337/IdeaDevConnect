dd9ed7e728ae34fc0d67058a190256c893f815e1
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-07-15 14:05:49 +0200

    front fix

--- views/templates/forum.php
+++ views/templates/forum.php
@@ -11,7 +11,7 @@
                     <h1 class="mb-4">Liste des Projets</h1>
 
                     <div class="row row-cols-1 row-cols-md-3 g-4">
-                        <div class="col">
+                    <div class="col">
                             <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title">Projet 1</h5>
